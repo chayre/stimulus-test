79af1879f150320e45c435221cbdf46730779cc8
seairs <sea.airs77@gmail.com>
2022-05-28 00:01:47 -0400

    added comments

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,9 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  // Reference the "source" target in HTML with sourceTarget
   static targets = [ "source" ]
+  // Reference the "supported" CSS class in HTML with supportedClass
   static classes = [ "supported" ]
 
+  // Upon connection, check the clipboard API to see if browser supports it
+  // If it does (result.state === "granted"), add the supported class (which displays the clipboard button)
+  // Otherwise don't add the supported class; default to not showing the clipboard button
   connect() {
     navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
       if (result.state === "granted") {
@@ -12,6 +17,7 @@ export default class extends Controller {
     });
   }
 
+  // After the button is clicked, copy the target text to the clipboard
   copy() {
     navigator.clipboard.writeText(this.sourceTarget.value)
   }
--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,12 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  // Targets the text input in the textbox
   static targets = [ "name" ]
 
+  //Display textbox input in console
   greet() {
      console.log(`Hello, ${this.name}!`)
   }
 
+  // Accessor method for the value in the textbox
   get name() {
     return this.nameTarget.value
   }
