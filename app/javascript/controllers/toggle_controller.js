64b43d408267918180e136b3f3d684cbf5bacb5e
seairs <sea.airs77@gmail.com>
2022-05-28 21:52:41 -0400

    comments

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,23 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  // Reference the "show" class that will be added to the element
+  // Reference the "show, hide, higlight" classes that will be added to the element
   static classes = [ "show", "hide", "highlight" ]
-  // Reference the target text
+  // Reference the targets of interest
   static targets = [ "text", "one", "two", "checkbox" ]
-
- 
 
   // When the toggle method is triggered, toggle the target's show class
   show() {
     this.textTarget.classList.toggle(this.showClass);
   }
+
   // When the toggle method is triggered, toggle the target's show class
   switch() {
     this.oneTarget.classList.toggle(this.hideClass);
     this.twoTarget.classList.toggle(this.showClass);
   }
 
+  // Toggles the highlight class onto the checkbox target
   highlight() {
     this.checkboxTarget.classList.toggle(this.highlightClass);
   }
