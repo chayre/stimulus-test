545fb8600d427570732083da324a06a9519ee7ce
seairs <sea.airs77@gmail.com>
2022-05-28 22:11:40 -0400

    comments

--- app/javascript/controllers/textwarn_controller.js
+++ app/javascript/controllers/textwarn_controller.js
@@ -1,13 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  //Reference the class which makes text red
+  // Reference the class which makes text red
   static classes = ["red"]
   // Reference the text, length elements in DOM
   static targets = [ "text", "length" ]
   
 
-  // When the toggle method is triggered, toggle the target's show class
+  // By default, tell the user how many characters they've written. If it's over 260 characters, make the text red. If it goes back under 260, remove the red
   warn() {
     this.lengthTarget.innerHTML = `${this.input}/280`
     if (this.input >= 260) {
@@ -18,7 +18,7 @@ export default class extends Controller {
   
   }
 
-  // When the toggle method is triggered, toggle the target's show class
+  // this.input will now return the length of the user-inputted text
   get input() {
     return this.textTarget.value.length
   }
